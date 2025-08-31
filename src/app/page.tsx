20448a0d5cd56ab4fc6bbbe98a4ca2f1cf1fbf55
Slivinskaya Vera <slivinskaya.vera@gmail.com>
2025-08-31 18:39:45 +0300

    check correct hooks' working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
+'use client';
+
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
+import { useCurrent } from '@/features/auth/api/use-current';
+import { useLogout } from '@/features/auth/api/use-logout';
 import { Button } from '@/components/ui/button';
 
 export default function Home() {
+  const router = useRouter();
+  const { data, isLoading } = useCurrent();
+  const { mutate } = useLogout();
+
+  useEffect(() => {
+    if (!data && !isLoading) {
+      router.push('/sign-in');
+    }
+  }, [data, isLoading, router]);
+
   return (
     <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
-      <Button variant='ghost' size='lg'>Click me</Button>
+      Only visible to authorized users.
+      <Button onClick={() => mutate()}>Logout</Button>
     </div>
   );
 }

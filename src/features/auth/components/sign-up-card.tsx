2d3badf48d6d6445fd83146315ab7e79e4b19de5
Slivinskaya Vera <slivinskaya.vera@gmail.com>
2025-09-01 16:26:27 +0300

    add conditions for ui elements

--- src/features/auth/components/sign-up-card.tsx
+++ src/features/auth/components/sign-up-card.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import z from 'zod';
 import { FcGoogle } from 'react-icons/fc';
 import { FaGithub } from 'react-icons/fa';
@@ -27,7 +29,7 @@ import { registerSchema } from '../schemas';
 import { useRegister } from '../api/use-register';
 
 const SignUpCard = () => {
-  const { mutate } = useRegister();
+  const { mutate, isPending } = useRegister();
 
   const form = useForm<z.infer<typeof registerSchema>>({
     resolver: zodResolver(registerSchema),
@@ -71,6 +73,7 @@ const SignUpCard = () => {
                   <FormControl>
                     <Input
                       {...field}
+                      disabled={isPending}
                       type='text'
                       placeholder='Enter your name'
                     />
@@ -88,6 +91,7 @@ const SignUpCard = () => {
                   <FormControl>
                     <Input
                       {...field}
+                      disabled={isPending}
                       type='email'
                       placeholder='Enter email address'
                     />
@@ -105,6 +109,7 @@ const SignUpCard = () => {
                   <FormControl>
                     <Input
                       {...field}
+                      disabled={isPending}
                       type='password'
                       placeholder='Enter password'
                     />
@@ -114,7 +119,7 @@ const SignUpCard = () => {
               )}
             />
 
-            <Button disabled={false} size='lg' className='w-full'>
+            <Button disabled={isPending} size='lg' className='w-full'>
               Sign Up
             </Button>
           </form>
@@ -125,7 +130,7 @@ const SignUpCard = () => {
       </div>
       <CardContent className='p-7 flex flex-col gap-y-4'>
         <Button
-          disabled={false}
+          disabled={isPending}
           variant='secondary'
           size='lg'
           className='w-full'
@@ -134,7 +139,7 @@ const SignUpCard = () => {
           Login with Google
         </Button>
         <Button
-          disabled={false}
+          disabled={isPending}
           variant='secondary'
           size='lg'
           className='w-full'

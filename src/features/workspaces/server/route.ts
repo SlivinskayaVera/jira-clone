8f0d1a814c54feaf6862ad1cabaa6164a2ab9649
Slivinskaya Vera <slivinskaya.vera@gmail.com>
2025-09-05 14:28:59 +0300

    update get workspaces route with uniq member

--- src/features/workspaces/server/route.ts
+++ src/features/workspaces/server/route.ts
@@ -1,16 +1,40 @@
 import { Hono } from 'hono';
 import { zValidator } from '@hono/zod-validator';
+import { ID, Query } from 'node-appwrite';
 
 import { sessionMiddleware } from '@/lib/session-middleware';
+import { generateInviteCode } from '@/lib/utils';
+import { MemberRole } from '@/features/members/types';
+
+import {
+  DATABASE_ID,
+  IMAGES_BUCKET_ID,
+  MEMBERS_ID,
+  WORKSPACE_ID,
+} from '@/config';
+
 import { createWorkspaceSchema } from '../schemas';
-import { DATABASE_ID, IMAGES_BUCKET_ID, WORKSPACE_ID } from '@/config';
-import { ID } from 'node-appwrite';
 
 const app = new Hono()
   .get('/', sessionMiddleware, async (c) => {
+    const user = c.get('user');
     const databases = c.get('databases');
 
-    const workspaces = await databases.listDocuments(DATABASE_ID, WORKSPACE_ID);
+    const members = await databases.listDocuments(DATABASE_ID, MEMBERS_ID, [
+      Query.equal('userID', user.$id),
+    ]);
+
+    if (members.total === 0) {
+      return c.json({ data: { documents: [], total: 0 } });
+    }
+
+    const workspaceIDs = members.documents.map((member) => member.workspaceID);
+
+    const workspaces = await databases.listDocuments(
+      DATABASE_ID,
+      WORKSPACE_ID,
+      [Query.orderDesc('$createdAt'), Query.contains('$id', workspaceIDs)]
+    );
 
     return c.json({ data: workspaces });
   })
@@ -52,8 +76,15 @@ const app = new Hono()
           name,
           userID: user.$id,
           imageUrl: uploadedImageUrl,
+          inviteCode: generateInviteCode(10),
         }
       );
+
+      await databases.createDocument(DATABASE_ID, MEMBERS_ID, ID.unique(), {
+        userID: user.$id,
+        workspaceID: workspace.$id,
+        role: MemberRole.ADMIN,
+      });
 
       return c.json({ data: workspace });
     }

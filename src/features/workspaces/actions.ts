df4e8d0de4a749425620042f76e4b86f3867bf77
Slivinskaya Vera <slivinskaya.vera@gmail.com>
2025-09-08 16:43:39 +0300

    create getWorkspaces action

---
+++ src/features/workspaces/actions.ts
@@ -0,0 +1,45 @@
+'use server';
+
+import { cookies } from 'next/headers';
+import { Account, Client, Databases, Query } from 'node-appwrite';
+
+import { AUTH_COOKIE } from '@/features/auth/constants';
+import { DATABASE_ID, MEMBERS_ID, WORKSPACE_ID } from '@/config';
+
+export const getWorkspaces = async () => {
+  try {
+    const client = new Client()
+      .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
+      .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!);
+
+    const session = await cookies().get(AUTH_COOKIE);
+
+    if (!session) return { documents: [], total: 0 };
+
+    client.setSession(session.value);
+
+    const databases = new Databases(client);
+    const account = new Account(client);
+    const user = await account.get();
+
+    const members = await databases.listDocuments(DATABASE_ID, MEMBERS_ID, [
+      Query.equal('userID', user.$id),
+    ]);
+
+    if (members.total === 0) {
+      return { documents: [], total: 0 };
+    }
+
+    const workspaceIDs = members.documents.map((member) => member.workspaceID);
+
+    const workspaces = await databases.listDocuments(
+      DATABASE_ID,
+      WORKSPACE_ID,
+      [Query.orderDesc('$createdAt'), Query.contains('$id', workspaceIDs)]
+    );
+
+    return workspaces;
+  } catch {
+    return { documents: [], total: 0 };
+  }
+};

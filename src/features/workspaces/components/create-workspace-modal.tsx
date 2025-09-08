f915b6ab68038266cfdf4f0a4e99bea1dc00caf5
Slivinskaya Vera <slivinskaya.vera@gmail.com>
2025-09-08 16:45:46 +0300

    create CreateWorkspaceModal component

---
+++ src/features/workspaces/components/create-workspace-modal.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import { ResponsiveModal } from '@/components/ui/responsive-modal';
+import { useCreateWorkspaceModal } from '../hooks/use-create-workspace-modal';
+import { CreateWorkspaceForm } from './create-workspace-form';
+
+export const CreateWorkspaceModal = () => {
+  const { isOpen, setIsOpen } = useCreateWorkspaceModal();
+
+  return (
+    <ResponsiveModal open={isOpen} onOpenChange={setIsOpen}>
+      <CreateWorkspaceForm />
+    </ResponsiveModal>
+  );
+};

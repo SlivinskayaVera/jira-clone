81b0d4f7d4d38aef3bb0fdf79abea7eb6892e125
Slivinskaya Vera <slivinskaya.vera@gmail.com>
2025-09-17 21:13:50 +0300

    Change paths in nav

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { SettingsIcon, UserIcon } from 'lucide-react';
 import Link from 'next/link';
 import {
@@ -6,7 +8,9 @@ import {
   GoHome,
   GoHomeFill,
 } from 'react-icons/go';
+import { usePathname } from 'next/navigation';
 import { cn } from '@/lib/utils';
+import { useWorkspaceId } from '@/features/workspaces/hooks/use-workspace-id';
 
 const routes = [
   {
@@ -36,14 +40,18 @@ const routes = [
 ];
 
 export const Navigation = () => {
+  const workspaceId = useWorkspaceId();
+  const pathname = usePathname();
+
   return (
     <ul className='flex flex-col'>
       {routes.map((item) => {
-        const isActive = false;
+        const fullHref = `/workspaces/${workspaceId}${item.href}`
+        const isActive = pathname === fullHref;
         const Icon = isActive ? item.activeIcon : item.icon;
 
         return (
-          <Link key={item.href} href={item.href}>
+          <Link key={item.href} href={fullHref}>
             <div
               className={cn(
                 'flex items-center gap-2.5 p-2.5 rounded-md font-medium hover:text-primary transition text-neutral-500',

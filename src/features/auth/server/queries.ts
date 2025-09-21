e2cee65f89936a1a12520a843a4d74962f54352f
Slivinskaya Vera <slivinskaya.vera@gmail.com>
2025-09-21 09:16:59 +0300

    import createSessionClient to auth query getCurrent

--- src/features/auth/server/queries.ts
+++ src/features/auth/server/queries.ts
@@ -1,23 +1,11 @@
 'use server';
 
-import { cookies } from 'next/headers';
-import { Account, Client } from 'node-appwrite';
-
-import { AUTH_COOKIE } from '../constants';
+import { createSessionClient } from '@/lib/appwrite';
 
 // TODO: создать HOC с проверкой авторизации 
 export const getCurrent = async () => {
   try {
-    const client = new Client()
-      .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
-      .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!);
-
-    const session = await cookies().get(AUTH_COOKIE);
-
-    if (!session) return null;
-
-    client.setSession(session.value);
-    const account = new Account(client);
+    const { account } = await createSessionClient();
 
     return await account.get();
   } catch {

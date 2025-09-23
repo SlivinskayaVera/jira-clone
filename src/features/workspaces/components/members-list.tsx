fbb6f7201e652c548eb7b415d2b054bb8e4505bd
Slivinskaya Vera <slivinskaya.vera@gmail.com>
2025-09-23 17:08:33 +0300

    Create MembersList component with logic

---
+++ src/features/workspaces/components/members-list.tsx
@@ -0,0 +1,137 @@
+'use client';
+
+import { ArrowLeftIcon, MoreVerticalIcon } from 'lucide-react';
+import Link from 'next/link';
+import { Fragment } from 'react';
+
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Button } from '@/components/ui/button';
+import { DottedSeparator } from '@/components/dotted-separator';
+
+import { useGetMembers } from '@/features/members/api/use-get-members';
+import { MemberAvatar } from '@/features/members/components/member-avatar';
+
+import { Separator } from '@/components/ui/separator';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+
+import { useDeleteMember } from '@/features/members/api/use-delete-member';
+import { useUpdateMember } from '@/features/members/api/use-update-member';
+import { MemberRole } from '@/features/members/types';
+import { useConfirm } from '@/hooks/use-confirm';
+import { useWorkspaceId } from '../hooks/use-workspace-id';
+
+export const MembersList = () => {
+  const workspaceId = useWorkspaceId();
+  const [DeleteDialog, confirmDelete] = useConfirm(
+    'Remove member',
+    'This member will be removed from the workspace.',
+    'destructive'
+  );
+  const { data } = useGetMembers({ workspaceId });
+
+  const { mutate: deleteMember, isPending: isDeletingMember } =
+    useDeleteMember();
+
+  const { mutate: updateMember, isPending: isUpdatingMember } =
+    useUpdateMember();
+
+  const isPending = isDeletingMember || isUpdatingMember;
+
+  const handleUpdateMember = (memberId: string, role: MemberRole) => {
+    updateMember({
+      json: { role },
+      param: { memberId },
+    });
+  };
+
+  const handleDeleteMember = async (memberId: string) => {
+    const ok = await confirmDelete();
+    if (!ok) return;
+
+    deleteMember(
+      { param: { memberId } },
+      {
+        onSuccess: () => {
+          window.location.reload();
+        },
+      }
+    );
+  };
+
+  return (
+    <Card className='w-full h-full border-none shadow-none'>
+      <DeleteDialog />
+      <CardHeader className='flex flex-row items-center gap-x-4 space-y-0'>
+        <Button asChild variant='secondary' size='sm'>
+          <Link href={`/workspaces/${workspaceId}`}>
+            <ArrowLeftIcon className='size-4 mr-2' />
+            Back
+          </Link>
+        </Button>
+        <CardTitle className='text-xl font-bold'>Members list</CardTitle>
+      </CardHeader>
+      <div className='px-7'>
+        <DottedSeparator />
+      </div>
+      <CardContent className='p-7'>
+        {data?.documents.map((member, index) => (
+          <Fragment key={member.$id}>
+            <div className='flex items-center gap-2'>
+              <MemberAvatar
+                classname='size-10'
+                fallbackClassName='text-lg'
+                name={member.name}
+              />
+              <div className='flex flex-col'>
+                <p className='text-sm font-medium'>{member.name}</p>
+                <p className='text-xs text-muted-foreground'>{member.email}</p>
+              </div>
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button className='ml-auto' variant='secondary' size='icon'>
+                    <MoreVerticalIcon className='size-4 text-muted-foreground' />
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                  <DropdownMenuItem
+                    className='font-medium'
+                    onClick={() =>
+                      handleUpdateMember(member.$id, MemberRole.ADMIN)
+                    }
+                    disabled={isPending}
+                  >
+                    Set as Administrator
+                  </DropdownMenuItem>
+                  <DropdownMenuItem
+                    className='font-medium'
+                    onClick={() =>
+                      handleUpdateMember(member.$id, MemberRole.MEMBER)
+                    }
+                    disabled={isPending}
+                  >
+                    Set as Member
+                  </DropdownMenuItem>
+                  <DropdownMenuItem
+                    className='font-medium text-amber-700'
+                    onClick={() => handleDeleteMember(member.$id)}
+                    disabled={isPending}
+                  >
+                    Remove {member.name}
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </div>
+            {index < data.documents.length - 1 && (
+              <Separator className='my-2.5' />
+            )}
+          </Fragment>
+        ))}
+      </CardContent>
+    </Card>
+  );
+};

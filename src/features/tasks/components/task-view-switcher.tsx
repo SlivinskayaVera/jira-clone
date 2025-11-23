70482b1649690a78b9e7644a7f980ed9337aaa3f
Slivinskaya Vera <slivinskaya.vera@gmail.com>
2025-11-23 09:04:34 +0300

    create useCreateTaskModal

--- src/features/tasks/components/task-view-switcher.tsx
+++ src/features/tasks/components/task-view-switcher.tsx
@@ -1,10 +1,15 @@
+'use client';
+
 import { PlusIcon } from 'lucide-react';
 import { TabsContent } from '@radix-ui/react-tabs';
 import { Button } from '@/components/ui/button';
 import { Tabs, TabsTrigger, TabsList } from '@/components/ui/tabs';
 import { DottedSeparator } from '@/components/dotted-separator';
+import { useCreateTaskModal } from '../hooks/use-create-task-modal';
 
 export const TaskViewSwitcher = () => {
+  const { open } = useCreateTaskModal();
+
   return (
     <Tabs className='flex-1 w-full border rounded-lg'>
       <div className='h-full flex flex-col overflow-auto p-4'>
@@ -20,7 +25,7 @@ export const TaskViewSwitcher = () => {
               Calendar
             </TabsTrigger>
           </TabsList>
-          <Button size='sm' className='w-full lg:w-auto'>
+          <Button size='sm' className='w-full lg:w-auto' onClick={open}>
             <PlusIcon className='size-4 mr-2' />
             New
           </Button>

ff317adc576a28ec2c7611e6bcb843b96001ed2b
Slivinskaya Vera <slivinskaya.vera@gmail.com>
2025-11-30 16:42:23 +0300

    create data columns

--- src/features/tasks/components/task-view-switcher.tsx
+++ src/features/tasks/components/task-view-switcher.tsx
@@ -11,6 +11,8 @@ import { useCreateTaskModal } from '../hooks/use-create-task-modal';
 import { useGetTasks } from '../api/use-get-tasks';
 import { useTaskFilters } from '../hooks/use-task-filters';
 import { DataFilters } from './data-filters';
+import { DataTable } from './data-table';
+import { columns } from './columns';
 
 export const TaskViewSwitcher = () => {
   const [{ status, assigneeId, projectId, dueDate }] = useTaskFilters();
@@ -61,7 +63,7 @@ export const TaskViewSwitcher = () => {
         ) : (
           <>
             <TabsContent value='table' className='mt-0'>
-              Data table
+              <DataTable columns={columns} data={tasks?.documents ?? []}/>
             </TabsContent>
             <TabsContent value='kanban' className='mt-0'>
               Data kanban
